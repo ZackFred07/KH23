a7a605e3989cdcf854feae048de736a7337d7b29
Zackary Kiener <kienerz07@icloud.com>
2023-10-06 23:58:46 -0400

    three.js stuff

--- frontend/src/components/Chatbar.jsx
+++ frontend/src/components/Chatbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React,{ useRef } from 'react'
+import * as THREE from 'three';
 
-const Chatbar = () => {
-  return (
-    <div>Chatbar</div>
-  )
+const Chatbar = ({ text, position }) => {
+  const textRef = useRef();
+
+  // Create a text geometry for the dialogue
+  const textGeometry = new THREE.TextGeometry(text, {
+    font: new THREE.FontLoader().parse('/path/to/font.json'), // Replace with your font file
+    size: 0.2,
+    height: 0.02,
+  });
+
+  // Create a material for the text
+  const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+  // Create a mesh for the text
+  const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+  textMesh.position.copy(position);
+  textRef.current.add(textMesh);
+
+  return <group ref={textRef} />;
+
 }
 
 export default Chatbar
--- frontend/src/pages/Canvas.jsx
+++ frontend/src/pages/Canvas.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader';
+import { OrbitControls } from 'react-orbit-controls';
+import * as THREE from 'three';
 
 const Canvas = () => {
   return (
-    <div>Canvas</div>
+    <Canvas camera={{ position: [0, 2, 5] }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 10, 0]} intensity={1} />
+      <CourtroomScene />
+    </Canvas>
   )
 }
 
-export default Canvas

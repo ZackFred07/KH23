72290da2b7dd46d0e4a5b9529d40aa41bcbb22d7
Zackary Kiener <64226837+ZackFred07@users.noreply.github.com>
2023-10-07 22:51:26 -0400

    change the icons of the login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,30 @@
-import LoginForm from './pages/login';
-import SignUpForm from './pages/register';
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-
-
+import LoginForm from "./pages/login";
+import SignUpForm from "./pages/register";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Game from "./pages/Game";
 
 export default function App() {
-  return (
-    
-      <div>
-        <div className='header'>
-        <a className="logo" href="/game">Game Name</a>
-        <nav className="navbar">
-          <a href="/home">Home</a>
-          <a href="/dashboard">Dashboard</a>
-          <a href="/">Login</a>
-          <a href='/register'>Register</a>
-        </nav>
-        </div>
-        <Router>
-
-        <Routes>
-        <Route exact path='/' element={<LoginForm/>}/>
-        <Route path='/register' element={<SignUpForm/>}/>
-        </Routes>
-        </Router>
-      </div>
-      
-  )
-  }
+	return (
+		<div className="App">
+			<div className="header">
+				<a className="logo" href="/game">
+					Game Name
+				</a>
+				<nav className="navbar">
+					<a href="/home">Home</a>
+					<a href="/dashboard">Dashboard</a>
+					<a href="/">Login</a>
+					<a href="/register">Register</a>
+				</nav>
+			</div>
+			<Router>
+				<Routes>
+					<Route exact path="/" element={<LoginForm />} />
+					<Route path="/register" element={<SignUpForm />} />
+					<Route path="/game" element={<Game />} />
+				</Routes>
+			</Router>
+		</div>
+	);
+}
